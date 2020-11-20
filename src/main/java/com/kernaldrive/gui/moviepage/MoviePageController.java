8fe60db6eca94638a68b881d73ede5b0eac865d0
Idrees Syed <idreesmsyed1@gmail.com>
2020-11-19 19:10:35 -0500

    Added basic cast and crew scrollvpane to the movie page

--- src/main/java/com/kernaldrive/gui/moviepage/MoviePageController.java
+++ src/main/java/com/kernaldrive/gui/moviepage/MoviePageController.java
@@ -3,8 +3,11 @@ package com.kernaldrive.gui.moviepage;
 
 import com.kernaldrive.gui.MainPageController;
 import com.kernaldrive.metadata.Movie;
+import info.movito.themoviedbapi.model.people.PersonCast;
+import info.movito.themoviedbapi.model.people.PersonCrew;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -25,11 +28,18 @@ import javafx.scene.paint.Stop;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoviePageController {
 
     private Movie movie;
     private String fontPath = "file:C:\\Users\\idree\\Documents\\Java Projects\\KernalDrive\\varela\\Varela-Regular.otf";
     private Font movieFont = Font.loadFont(fontPath, 40);
+    private ArrayList<ImageView> castImages;
+    private ArrayList<ImageView> crewImages;
+    private List<PersonCast> cast;
+    private List<PersonCrew> crew;
 
     @FXML private ScrollPane scrollPane;
     @FXML private Pane moviePane;
@@ -44,15 +54,21 @@ public class MoviePageController {
     @FXML private Label genreLabel;
     @FXML private Label taglineLabel;
     @FXML private Label synopsisLabel;
+    @FXML private HBox castHBox;
+    @FXML private HBox crewHBox;
+
 
 
     public void setMovie(Movie movie){
         this.movie = movie;
+        cast = movie.getCast();
+        crew = movie.getCrew();
     }
 
     public void setLabels(){
         //scrollPane.setStyle("-fx-background: linear-gradient(to bottom left, rgba(40,43,82, 0.8) 20% , rgba(21,102,123, 0.8))");
         //moviePane.setStyle("-fx-background-color: rgba(217, 217, 217, 0.2); -fx-background-radius: 10;");
+        moviePane.setStyle("-fx-border-color: BLACK");
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         double  screenWidth = screenBounds.getWidth();
         double screenHeight = screenBounds.getHeight();
@@ -60,7 +76,7 @@ public class MoviePageController {
         posterImage.setImage(new Image(movie.getPosterPath()));
         Image img = new Image(movie.getBannerPath());
         ImageView bannerI = new ImageView(img);
-        bannerI.setFitHeight(543);
+        bannerI.setPreserveRatio(true);
         bannerI.setFitWidth(1000);
         bannerI.setSmooth(true);
         WritableImage croppedImage = bannerI.snapshot(null, null);
@@ -73,15 +89,10 @@ public class MoviePageController {
         GraphicsContext gc = bannerCanvas.getGraphicsContext2D();
         gc.setEffect(new GaussianBlur(50));
         gc.setFill(Color.BLACK);
-        gc.fillRect(40, 40, w-70, h-70);
+        gc.fillRect(40, 40, w-80, h-100);
         gc.setEffect(null);
         gc.setGlobalBlendMode(BlendMode.SRC_ATOP);
         gc.drawImage(croppedImage, 0, 0);
-
-
-
-        //bannerImageView.setCache(true);
-        //fixBlurryText(scrollPane);
 
         titleLabel.setText(movie.getTitle());
         //titleLabel.setFont(movieFont);
@@ -102,8 +113,81 @@ public class MoviePageController {
         synopsisLabel.setText(movie.getOverview());
         synopsisLabel.setMaxWidth(800);
         synopsisLabel.setWrapText(true);
+        loadCastCrew();
 
 
+    }
+
+    public void loadCastCrew(){
+        castImages = new ArrayList<>();
+        crewImages = new ArrayList<>();
+        for(int i=0; i<cast.size(); i++){
+            VBox test = addCast(cast.get(i));
+            castHBox.getChildren().addAll(test);
+        }
+        for(int i=0; i<crew.size(); i++){
+            VBox test = addCrew(crew.get(i));
+            crewHBox.getChildren().addAll(test);
+        }
+
+        Thread castThread = new Thread(new Runnable() {
+            public void run(){
+                for(int i=0; i<cast.size(); i++){
+                    if(cast.get(i).getProfilePath() != "") {
+                        castImages.get(i).setImage(new Image("https://image.tmdb.org/t/p/w185" + cast.get(i).getProfilePath()));
+                    }
+                }
+            }
+        });
+        Thread crewThread = new Thread(new Runnable() {
+            public void run(){
+                for(int i=0; i<crew.size(); i++){
+                    if(crew.get(i).getProfilePath() != "") {
+                        crewImages.get(i).setImage(new Image("https://image.tmdb.org/t/p/w185" + crew.get(i).getProfilePath()));
+                    }
+                }
+            }
+        });
+        castThread.start();
+        crewThread.start();
+    }
+
+    public VBox addCast(PersonCast person){
+        VBox test = new VBox();
+        test.setPadding(new Insets(10,10,10,10));
+        Image personImg = new Image("file:C:\\Users\\idree\\Documents\\Java Projects\\KernalDrive\\cast.jpg");
+        ImageView personImage = new ImageView(personImg);
+        //personImage.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);");
+        castImages.add(personImage);
+        personImage.setFitWidth(100);
+        personImage.setPreserveRatio(true);
+        Label personName = new Label(person.getName());
+        personName.setMaxWidth(100);
+        personName.setAlignment(Pos.CENTER);
+        Label personRole = new Label(person.getCharacter());
+        personRole.setMaxWidth(100);
+        personRole.setAlignment(Pos.CENTER);
+        test.getChildren().addAll(personImage, personName, personRole);
+        return test;
+    }
+
+    public VBox addCrew(PersonCrew person){
+        VBox test = new VBox();
+        test.setPadding(new Insets(10,10,10,10));
+        Image personImg = new Image("file:C:\\Users\\idree\\Documents\\Java Projects\\KernalDrive\\cast.jpg");
+        ImageView personImage = new ImageView(personImg);
+        //personImage.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.8), 10, 0, 0, 0);");
+        crewImages.add(personImage);
+        personImage.setFitWidth(100);
+        personImage.setPreserveRatio(true);
+        Label personName = new Label(person.getName());
+        personName.setMaxWidth(100);
+        personName.setAlignment(Pos.CENTER);
+        Label personRole = new Label(person.getDepartment());
+        personRole.setMaxWidth(100);
+        personRole.setAlignment(Pos.CENTER);
+        test.getChildren().addAll(personImage, personName, personRole);
+        return test;
     }
 
 
--- src/main/java/com/kernaldrive/gui/moviepage/MoviePageTester.java
+++ src/main/java/com/kernaldrive/gui/moviepage/MoviePageTester.java
@@ -52,6 +52,7 @@ public class MoviePageTester extends Application {
         primaryStage.setScene(moviePage);
         primaryStage.initStyle(StageStyle.UTILITY);
         primaryStage.show();
+
         MoviePageController moviePageController = loader.getController();
         TmdbFilenameSearch tmdbFilenameSearch = new TmdbFilenameSearch();
         TmdbMovieExtractor tmdbMovieExtractor = new TmdbMovieExtractor();
@@ -59,35 +60,6 @@ public class MoviePageTester extends Application {
         Movie movie = tmdbMovieExtractor.extractMovieInfo("", movieID);
         moviePageController.setMovie(movie);
         moviePageController.setLabels();
-
-        //List<String> imgPaths = this.getParameters().getUnnamed();
-
- /*       Image img = new Image(movie.getBannerPath());
-        //WritableImage croppedImage = new WritableImage(img.getPixelReader(), 0, 0, 100, 100);
-
-        int w = (int) img.getWidth();
-        int h = (int) img.getHeight();
-
-        Canvas canvas = new Canvas(w, h);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.setEffect(new GaussianBlur(50));
-        gc.setFill(Color.BLACK);
-        gc.fillRect(30, 30, w-60, h-60);
-        gc.setEffect(null);
-        gc.setGlobalBlendMode(BlendMode.SRC_ATOP);
-        gc.drawImage(img, 0, 0);
-
-        VBox pane = new VBox(canvas);
-        pane.setStyle("-fx-background-color: linear-gradient(to bottom left, rgba(40,43,82, 0.8) 20% , rgba(21,102,123, 0.8))");
-
-        pane.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, CornerRadii.EMPTY, Insets.EMPTY)));
-
-        Scene scene = new Scene(pane);
-        primaryStage.setTitle("Image Blend");
-        primaryStage.setScene(scene);
-        primaryStage.setMinWidth(300);
-        primaryStage.setMinHeight(300);
-        primaryStage.show();*/
 
     }
 }
