63f929e189e4b53346aa2a251d60f414b3129bbc
Ibraheem Aloran <ibraheemaloran@gmail.com>
2020-11-21 21:44:19 -0500

    Added fast forward and rewind buttons and made the video play instantly as well as removed the original play button

--- src/main/java/com/kernaldrive/videoplayer/mediaPlayer.java
+++ src/main/java/com/kernaldrive/videoplayer/mediaPlayer.java
@@ -1,7 +1,7 @@
 package com.kernaldrive.videoplayer;
 
 
-import java.awt.BorderLayout;
+/*import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
@@ -21,11 +21,11 @@ public class mediaPlayer extends JFrame {
    private static final String TITLE = "My First Media Player";
    private static String VIDEO_PATH;//= "Z:\\PhonyStark\\Star Wars Episode I The Phantom Menace (1999) BONUS DISC [1080p BluRay DD 2.0 REMUX AVC FGT]\\Complete Podrace Grid Sequence.mkv";
    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
-   private JButton playButton, pauseButton;
+   private JButton playButton, rewindButton, pauseButton;
    private JToggleButton toggleButton;
 
    public mediaPlayer(String path) {
-      //super(title);
+      super("Media Player");
       mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
       mediaPlayerComponent.mediaPlayer().fullScreen().strategy(new AdaptiveFullScreenStrategy(this));
       VIDEO_PATH = path;
@@ -51,6 +51,8 @@ public class mediaPlayer extends JFrame {
       controlsPane.add(pauseButton);
       toggleButton = new JToggleButton("Full Screen");
       controlsPane.add(toggleButton);
+      rewindButton = new JButton("Rewind");
+      controlsPane.add(rewindButton);
       contentPane.add(controlsPane, BorderLayout.SOUTH);
       playButton.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
@@ -67,11 +69,16 @@ public class mediaPlayer extends JFrame {
             mediaPlayerComponent.mediaPlayer().fullScreen().toggle();
          }
       });
+      rewindButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().controls().skipTime(-14000);
+         }
+      });
       this.setContentPane(contentPane);
       this.setVisible(true);
    }
    public void loadVideo(String path) {
-      mediaPlayerComponent.mediaPlayer().media().startPaused(path);   	
+      mediaPlayerComponent.mediaPlayer().media().start(path);
    }
    public void start(){
       try {
@@ -85,6 +92,115 @@ public class mediaPlayer extends JFrame {
       initialize();
       setVisible(true);
       loadVideo(VIDEO_PATH);
+   }
+   public static void main (String[] args){
+      mediaPlayer mp = new mediaPlayer("jojo.mp4");
+      mp.start();
+   }
+}
+*/
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.JToggleButton;
+
+import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.embedded.fullscreen.adaptive.AdaptiveFullScreenStrategy;
+
+public class mediaPlayer extends JFrame {
+   private static final long serialVersionUID = 1L;
+   //private static final String TITLE = "My First Media Player";
+   private static String VIDEO_PATH;//= "Z:\\PhonyStark\\Star Wars Episode I The Phantom Menace (1999) BONUS DISC [1080p BluRay DD 2.0 REMUX AVC FGT]\\Complete Podrace Grid Sequence.mkv";
+   private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+   private JButton playButton, rewindButton, pauseButton, skipButton;
+   private JToggleButton toggleButton;
+
+   public mediaPlayer(String path) {
+      super("Media Player");
+      mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+      mediaPlayerComponent.mediaPlayer().fullScreen().strategy(new AdaptiveFullScreenStrategy(this));
+      VIDEO_PATH = path;
+   }
+   public void initialize() {
+      this.setBounds(100, 100, 600, 400);
+      this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      this.addWindowListener(new WindowAdapter() {
+         @Override
+         public void windowClosing(WindowEvent e) {
+            mediaPlayerComponent.release();
+            //System.exit(0);
+         }
+      });
+      JPanel contentPane = new JPanel();
+      contentPane.setLayout(new BorderLayout());
+      contentPane.add(mediaPlayerComponent, BorderLayout.CENTER);
+
+      JPanel controlsPane = new JPanel();
+      playButton = new JButton("Play");
+      //controlsPane.add(playButton);
+      pauseButton = new JButton("Pause/Play");
+      controlsPane.add(pauseButton);
+      toggleButton = new JToggleButton("Full Screen");
+      controlsPane.add(toggleButton);
+      rewindButton = new JButton("<<");
+      controlsPane.add(rewindButton);
+      skipButton = new JButton(">>");
+      controlsPane.add(skipButton);
+      contentPane.add(controlsPane, BorderLayout.SOUTH);
+      playButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().controls().play();
+         }
+      });
+      pauseButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().controls().pause();
+         }
+      });
+      toggleButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().fullScreen().toggle();
+         }
+      });
+      rewindButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().controls().skipTime(-5000);
+         }
+      });
+      skipButton.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            mediaPlayerComponent.mediaPlayer().controls().skipTime(5000);
+         }
+      });
+      this.setContentPane(contentPane);
+      this.setVisible(true);
+   }
+   public void loadVideo(String path) {
+      mediaPlayerComponent.mediaPlayer().media().start(path);
+   }
+   public void start(){
+      try {
+         UIManager.setLookAndFeel(
+                 UIManager.getSystemLookAndFeelClassName());
+      }
+      catch (Exception e) {
+         System.out.println(e);
+      }
+      //mediaPlayer application = new mediaPlayer();
+      initialize();
+      setVisible(true);
+      loadVideo(VIDEO_PATH);
+   }
+   public static void main (String[] args){
+      mediaPlayer mp = new mediaPlayer("jojo.mp4");
+      mp.start();
    }
 }
 
